cc208b2a491bc517c8542061e2dd3bf1a9bc8623
SergioGBosco <sergiogibosco@gmail.com>
2025-07-19 18:52:31 +0200

    feat: add Layout html

--- js/script.js
+++ js/script.js
@@ -6,12 +6,29 @@ const mail = document.getElementById(`containerEmail`)
 
 let randomEmail = `https://flynn.boolean.careers/exercises/api/random/mail`
 
+let randomSentence = `https://flynn.boolean.careers/exercises/api/random/sentence`
 
-// function generateEmail() {
-for (let i = 0; i < 10; i++) {
-  axios.get(randomEmail).then(resp => {
-    const email = resp.data.response
-    mail.innerHTML += `<div>${email}</div>`
-  })
+let data = new Date;
+let day = data.getDate();
+let mese = data.getMonth() + 1;
+let hour = data.getHours();
+let minuts = data.getMinutes();
+
+
+let received = `${day}/${mese} ${hour}:${minuts}`
+
+function generateEmail() {
+  for (let i = 0; i < 10; i++) {
+    axios.get(randomEmail).then(resp => {
+      const email = resp.data.response
+      mail.innerHTML += `     <tr>
+                <td>${email}</td>
+                <td>Otto</td>
+                <td class ="text-center">${received}</td>
+              </tr>`
+    })
+  }
 }
-// }
+//Chiamata della funzione 
+generateEmail();
+
